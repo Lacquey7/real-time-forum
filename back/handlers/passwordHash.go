296a58072d4fab872f42ea4f5c3f41347ebd270b
MrFruchard <Romain2702@hotmail.fr>
2025-02-11 15:47:41 +0100

    comment

--- back/handlers/passwordHash.go
+++ back/handlers/passwordHash.go
@@ -4,6 +4,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// HashPassword Permet de hasher un mot de passe
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
