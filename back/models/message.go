5a6eb025c80636e4f35e3250f619bdea5392abe1
squibel <samquibel@icloud.com>
2025-02-13 13:55:03 +0100

    login a jour

---
+++ back/models/message.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/gorilla/websocket"
+
+type Message struct {
+	Type    string          `json:"type"`
+	Content string          `json:"content"`
+	Sender  *websocket.Conn `json:"-"`
+}
